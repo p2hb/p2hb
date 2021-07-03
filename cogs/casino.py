69506afd4c3dc8b8a6eb3bc3568edae66dbff71a
ItsAmeYushi <ayushkt@icloud.com>
2021-07-03 19:14:25 -0400

    Add blackjack

--- cogs/casino.py
+++ cogs/casino.py
@@ -260,6 +260,280 @@ class Casino(commands.Cog):
             return await ctx.send(embed=embed)
 
     @checks.has_started()
+    @commands.max_concurrency(1, commands.BucketType.user)
+    @commands.command(aliases=["bj"])
+    async def blackjack(self, ctx, start="about", amount=10):
+        if amount <= 0:
+            return await ctx.send("Nice Try")
+
+        if start == "start":
+            member = await self.bot.mongo.fetch_member_info(ctx.author)
+            bal = member.balance
+
+            if bal < amount:
+                return await ctx.send(
+                    f"You don't have enough tokens to play (min: {amount})"
+                )
+            # deck
+            cards = ["A♠️", "A♥️", "A♣️", "A♦️",
+            "2♠️", "2♥️", "2♣️", "2♦️",
+            "3♠️", "3♥️", "3♣️", "3♦️",
+            "4♠️", "4♥️", "4♣️", "4♦️",
+            "5♠️", "5♥️", "5♣️", "5♦️",
+            "6♠️", "6♥️", "6♣️", "6♦️",
+            "7♠️", "7♥️", "7♣️", "7♦️",
+            "8♠️", "8♥️", "8♣️", "8♦️",
+            "9♠️", "9♥️", "9♣️", "9♦️",
+            "10♠️", "10♥️", "10♣️", "10♦️",
+            "J♠️", "J♥️", "J♣️", "J♦️",
+            "Q♠️", "Q♥️", "Q♣️", "Q♦️",
+            "K♠️", "K♥️", "K♣️", "K♦️"]
+            random.shuffle(cards)
+            
+            # helper to get value of a hand
+            def value(h):
+                cs=h.split()
+                v=0
+                a=0
+                for c in cs:
+                    cv = c[0]
+                    if cv == "1" or cv == "J" or cv == "Q" or cv == "K":
+                        cv = "10"
+                    elif cv == "A":
+                        cv = "11"
+                        a += 1
+                    elif cv == "?":
+                        cv = "0"
+                    cv = int(cv)
+                    v += cv
+                while v>21:
+                    if a>0:
+                        a -= 1
+                        v -= 10
+                    else:
+                        break
+                return v
+            
+            def soft(h):
+                cs=h.split()
+                v=0
+                a=0
+                for c in cs:
+                    cv = c[0]
+                    if cv == "1" or cv == "J" or cv == "Q" or cv == "K":
+                        cv = "10"
+                    elif cv == "A":
+                        cv = "11"
+                        a += 1
+                    elif cv == "?":
+                        cv = "0"
+                    cv = int(cv)
+                    v += cv
+                while v>21:
+                    if a>0:
+                        a -= 1
+                        v -= 10
+                    else:
+                        break
+                return a>0
+            
+            # initialize variables
+            stay=False
+            hand=cards[0]+"  "+cards[1]
+            hhit = 2
+            dealer=cards[51]+"  ??"
+            dhit = 49
+            
+            # player blackjack
+            if value(hand)==21:
+                dealer=cards[51]+"  "+cards[50]
+                # both blackjack (= draw)
+                if value(dealer)==21:
+                    embed = discord.Embed(
+                        title=f"Blackjack — {amount}",
+                        description="You drew!",
+                        color=0xEB4634,
+                    )
+                    embed.add_field(
+                    name="Hand",
+                    value=f"{hand}\nValue: {value(hand)}",
+                    )
+                    embed.add_field(
+                    name="Dealer",
+                    value=f"{dealer}\nValue: {value(dealer)}",
+                    )
+                    embed.add_field(
+                        name="Winnings", value=f"You won **{amount} tokens.**", inline=False
+                    )
+                    return await ctx.send(f"> <@!{ctx.author.id}>", embed=embed)
+                # player blackjack and wins
+                else:
+                    embed = discord.Embed(
+                        title=f"Blackjack — {amount}",
+                        description="You won!",
+                        color=0xEB4634,
+                    )
+                    embed.add_field(
+                    name="Hand",
+                    value=f"{hand}\nValue: {value(hand)}",
+                    )
+                    embed.add_field(
+                    name="Dealer",
+                    value=f"{dealer}\nValue: {value(dealer)}",
+                    )
+                    await self.bot.mongo.update_member(
+                        ctx.author, {"$inc": {"balance": amount}}
+                    )
+                    embed.add_field(
+                        name="Winnings", value=f"You won **{amount*2} tokens.**", inline=False
+                    )
+                    return await ctx.send(f"> <@!{ctx.author.id}>", embed=embed)
+                
+            
+            # reaction check
+            def check(reaction, react_user):
+                if (react_user.id == ctx.author.id and (str(reaction.emoji) == "🛑") and reaction.message.id == confirm_message.id):
+                    stay=True
+                return (
+                    react_user.id == ctx.author.id
+                    and (str(reaction.emoji) == "🛑" or str(reaction.emoji) == "👊")
+                    and reaction.message.id == confirm_message.id
+                )
+            
+            # loop through turns until bust or stand
+            while (not stay):
+            
+                embed = discord.Embed(
+                    title=f"Blackjack — {amount}",
+                    description="React with 👊 to hit and 🛑 to stand.",
+                    color=0xEB4634,
+                )
+                embed.add_field(
+                name="Hand",
+                value=f"{hand}\nValue: {value(hand)}",
+                )
+                embed.add_field(
+                name="Dealer",
+                value=f"{dealer}\nValue: {value(dealer)}",
+                )
+                prev = await ctx.send(f"> <@!{ctx.author.id}>", embed=embed)
+                
+                await prev.add_reaction("👊")
+                await prev.add_reaction("🛑")
+                try:
+                    conf = await self.bot.wait_for("reaction_add", timeout=10, check=check)
+                except asyncio.TimeoutError:
+                    await ctx.send("Took too long to respond, automatically standing.")
+                    stay=True
+                if (not stay):
+                    hand+="  "+cards[hhit]
+                    hhit+=1
+                    if (value(hand)>21):
+                        stay=True
+            
+            # fix dealer hand
+            dealer=cards[51]+"  "+cards[50]
+            
+            # bust
+            if value(hand)>21:
+                embed = discord.Embed(
+                    title=f"Blackjack — {amount}",
+                    description="You busted!",
+                    color=0xEB4634,
+                )
+                embed.add_field(
+                name="Hand",
+                value=f"{hand}\nValue: {value(hand)}",
+                )
+                embed.add_field(
+                name="Dealer",
+                value=f"{dealer}\nValue: {value(dealer)}",
+                )
+                await self.bot.mongo.update_member(
+                    ctx.author, {"$inc": {"balance": -1 * amount}}
+                )
+                embed.add_field(
+                    name="Winnings", value=f"You won **0 tokens.**", inline=False
+                )
+                return await ctx.send(f"> <@!{ctx.author.id}>", embed=embed)
+            else:
+                # deal dealer's cards
+                while(value(dealer)<17 or (value(dealer)==17 and soft(dealer))):
+                    dealer+="  "+cards[dhit]
+                    dhit-=1
+                # dealer bust or player won
+                if value(dealer)>21 or value(hand)>value(dealer):
+                    embed = discord.Embed(
+                        title=f"Blackjack — {amount}",
+                        description="You won!",
+                        color=0xEB4634,
+                    )
+                    embed.add_field(
+                    name="Hand",
+                    value=f"{hand}\nValue: {value(hand)}",
+                    )
+                    embed.add_field(
+                    name="Dealer",
+                    value=f"{dealer}\nValue: {value(dealer)}",
+                    )
+                    await self.bot.mongo.update_member(
+                        ctx.author, {"$inc": {"balance": amount}}
+                    )
+                    embed.add_field(
+                        name="Winnings", value=f"You won **{amount*2} tokens.**", inline=False
+                    )
+                    return await ctx.send(f"> <@!{ctx.author.id}>", embed=embed)
+                # draw
+                elif value(hand)==value(dealer):
+                    embed = discord.Embed(
+                        title=f"Blackjack — {amount}",
+                        description="You drew!",
+                        color=0xEB4634,
+                    )
+                    embed.add_field(
+                    name="Hand",
+                    value=f"{hand}\nValue: {value(hand)}",
+                    )
+                    embed.add_field(
+                    name="Dealer",
+                    value=f"{dealer}\nValue: {value(dealer)}",
+                    )
+                    embed.add_field(
+                        name="Winnings", value=f"You won **{amount} tokens.**", inline=False
+                    )
+                    return await ctx.send(f"> <@!{ctx.author.id}>", embed=embed)
+                # player lost
+                else:
+                    embed = discord.Embed(
+                        title=f"Blackjack — {amount}",
+                        description="You lost!",
+                        color=0xEB4634,
+                    )
+                    embed.add_field(
+                    name="Hand",
+                    value=f"{hand}\nValue: {value(hand)}",
+                    )
+                    embed.add_field(
+                    name="Dealer",
+                    value=f"{dealer}\nValue: {value(dealer)}",
+                    )
+                    await self.bot.mongo.update_member(
+                        ctx.author, {"$inc": {"balance": -1 * amount}}
+                    )
+                    embed.add_field(
+                        name="Winnings", value=f"You won **0 tokens.**", inline=False
+                    )
+                    return await ctx.send(f"> <@!{ctx.author.id}>", embed=embed)
+
+        else:
+            embed = discord.Embed(
+                title=f"Blackjack",
+                description=f"You play blackjack. You get two cards and can choose to either hit to get another card or stand to end your turn. Aces are 11 or 1, whichever makes your hand better. Jacks, Queens, and Kings are 10.\nThe dealer will also play and if you get a higher total without going over 21 or the dealer busts, you win 2x the amount you enetered with. Do `{ctx.prefix}blackjack start <amount>` to play!",
+                color=0xEB4634,
+            )
+            return await ctx.send(embed=embed)
+
+    @checks.has_started()
     @commands.guild_only()
     @commands.max_concurrency(1, commands.BucketType.user)
     @commands.command(aliases=["s"])
